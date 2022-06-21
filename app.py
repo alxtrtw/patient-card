1cbfcc2cb87cea850591fe9a70c0df3839e95b75
alxtrtw <alext45d@gmail.com>
2022-06-21 23:05:48 +0200

    added gender

--- app.py
+++ app.py
@@ -11,29 +11,31 @@ def parse_admission_date(str_date: str):
     return datetime(*date_int_tuple)
 
 class Patient():
-    def __init__(self, id, name, admission_date, y, events=['konsultacja lekarska']):
-        self.id = id
-        self.name = name
-        self.admission_date = parse_admission_date(admission_date)
+    def __init__(self, patient):
+        self.id = patient.get('id')
+        self.name = patient.get_by_path('name.0.family') + ' ' + patient.get_by_path('name.0.given.0')
+        self.gender = "male"
+        self.birthDate = "2018-6-1"
+
+        self.admission_date = parse_admission_date("2018-6-1")
         self.admissionDate = self.admission_date.date()
-        self.y = y
-        self.events = events
+        self.events = ['badanie tomografem', 'nastawianie kości','wypadek samochodowy']
 
     def set_admission_date(self, date_str: str):
         self.admission_date = parse_admission_date(date_str)
         self.admissionDate = self.admission_date.date()
 
-def fhir_to_patient(patient):
-    return Patient(patient.get('id'),
-            patient.get_by_path('name.0.family') + ' ' + patient.get_by_path('name.0.given.0'),
-             "2018-6-1", "y", ['wywiad', 'diagnostyka', 'badanie rtg'])  
+# def fhir_to_patient(patient):
+#     return Patient(patient.get('id'),
+#             patient.get_by_path('name.0.family') + ' ' + patient.get_by_path('name.0.given.0'),
+#              "2018-6-1", "y", ['wywiad', 'diagnostyka', 'badanie rtg'])  
 
-patientargs = ("0", "Alex", "2018-6-1", "y", ['wywiad', 'diagnostyka', 'badanie rtg']), \
-    ("1", "Piotr", "2018-1-13", "y", ['badanie tomografem', 'nastawianie kości','wypadek samochodowy']), \
-    ("2", "Maciej", "2018-3-3", "y", ['zgon', 'podanie środków przeciwpromiennych']), \
-    ("3", "Stachu", "2018-11-15", "y", ['operacja kolana', 'wymiana insuliny']) 
+# patientargs = ("0", "Alex", "2018-6-1", "y", ['wywiad', 'diagnostyka', 'badanie rtg']), \
+#     ("1", "Piotr", "2018-1-13", "y", ['badanie tomografem', 'nastawianie kości','wypadek samochodowy']), \
+#     ("2", "Maciej", "2018-3-3", "y", ['zgon', 'podanie środków przeciwpromiennych']), \
+#     ("3", "Stachu", "2018-11-15", "y", ['operacja kolana', 'wymiana insuliny']) 
 
-database = [Patient(*args) for args in patientargs]
+# database = [Patient(*args) for args in patientargs]
 
 client = fhirpy.AsyncFHIRClient(
         'http://localhost:8080/baseR4/',
@@ -49,11 +51,12 @@ async def index():
 
     patients_list = []
     for patient in patients:
-        patients_list.append(fhir_to_patient(patient))
+        patients_list.append(Patient(patient))
     return render_template("index.html", patients=patients_list)
 
 @app.route('/insert', methods=['POST'])
 def insert():
+    # doesnt work
     if request.method == 'POST':
 
         name = request.form['name']
@@ -68,7 +71,7 @@ def insert():
 
 @app.route('/update', methods = ['GET', 'POST'])
 def update():
-
+    # doesnt work
     if request.method == 'POST':
         patient = [x for x in database if x.id == request.form.get('id')][0]
         patient.name = request.form['name']
@@ -80,6 +83,7 @@ def update():
 
 @app.route('/delete/<id>/', methods = ['GET', 'POST'])
 def delete(id):
+    # doesnt work
     patient = [x for x in database if x.id == id][0]
     database.remove(patient)
 
@@ -91,8 +95,7 @@ async def details(id):
     # app.logger.info(f"[LOGS] types: {type(patients_list[0])}, {type(id)}")
     # patient = [x for x in patients_list if x.id == id][0]
     patients = await client.resources('Patient').search(_id=[id]).fetch_all()
-    patient = fhir_to_patient(patients[0])
-    return render_template("details.html", patient=patient)
+    return render_template("details.html", patient=Patient(patients[0]))
 
 if __name__ == "__main__":
     app.run(debug=True)
